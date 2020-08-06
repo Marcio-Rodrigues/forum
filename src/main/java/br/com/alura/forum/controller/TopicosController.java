68b2d1a68ce4f44fdd29e2833f73730c8a2022eb
marci <marci@DESKTOP-K18THHT>
2020-08-06 20:30:58 -0300

    modelo criado e adição de dependencias

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.model.Topico;
+
+@RestController
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	public List<Topico> lista(){
+		Topico topico = new Topico("Duvida2", "Duvida com Spring", new Curso("Spring", "Programação"));
+		return Arrays.asList(topico, topico, topico);
+	}
+}
