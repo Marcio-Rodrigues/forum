f9af14974308b85ac9f3ec21d448356a30347d6b
marci <marci@DESKTOP-K18THHT>
2020-08-08 14:13:16 -0300

    fazendo busca com filtro

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -17,12 +17,17 @@ public class TopicosController {
 	private TopicoRepository topicoRepository;
 	
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
+	public List<TopicoDto> lista(String nomeCurso){
 //		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 //		
 //		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 		
-		List<Topico> topicos = topicoRepository.findAll();
-		return TopicoDto.converter(topicos);
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
 }
